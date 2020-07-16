2ec784cfc28d6f8b2d2d48afcf82a0aede29340b
smallgeek <smallgeek@outlook.jp>
2020-07-16 11:21:21 +0900

    同じピンを表示しないように履歴を持たせた

--- src/content_scripts.ts
+++ src/content_scripts.ts
@@ -23,49 +23,62 @@ async function applyFunctionView() {
     div.appendChild(button);
     bar.appendChild(div);
 
-    // 現在の実行IDを取得
-    let timeoutId = (await browser.storage.local.get("timeoutId")).timeoutId;
+    const data = (await browser.storage.local.get("drawingData")).drawingData;
 
     // 実行中（前のピンからの遷移）の場合
-    if (timeoutId) {
-        timeoutId = startTimer();
+    if (data.timeoutId) {
+        data.timeoutId = startTimer(data);
         button.innerText = "停止";
-        await browser.storage.local.set({ timeoutId: timeoutId });
+        await browser.storage.local.set({ drawingData: data });
     } else {
         button.innerText = "開始";
     }
 
     button.onclick = async e => {
         // 実行中でなければ
-        if (!timeoutId) {
-            timeoutId = startTimer();
+        if (!data.timeoutId) {
+            // 自動遷移を初期化する
+            data.histories = [];
+            data.timeoutId = startTimer(data);
             button.innerText = "停止";
         } else {
-            clearTimeout(timeoutId);
-            timeoutId = 0;
+            clearTimeout(data.timeoutId);
+            data.timeoutId = 0;
             button.innerText = "開始";
 
             //TODO: ピンの一覧を表示する
         }
 
-        await browser.storage.local.set({ timeoutId: timeoutId });
+        await browser.storage.local.set({ drawingData: data });
     };
 }
 
 /**
  * タイマーを開始する
  */
-function startTimer() {
-    return setTimeout(() => {
+function startTimer(data: any) {
+    return setTimeout(async () => {
+        // ピンを履歴に保存する
+        data.histories.push(document.location.href.replace("https://pinterest.jp/", ""));
+
         // 次のピンを取得
         //TODO: ボタンを押してから表示したピンは除く
-        const wrapper = document.querySelector("[data-test-id='pinWrapper']");
-        const nextPin = wrapper?.querySelector("a");
+        const wrappers = document.querySelectorAll("[data-test-id='pinWrapper']");
 
-        if (nextPin) {
-            //TODO: ピンを履歴に保存する
-            document.location.href = nextPin.href;
+        for (const wrapper of Array.from(wrappers)) {
+            // 履歴に無いものを次のピンにする。
+            const anchors = Array.from(wrapper.getElementsByTagName("a"));
+            const nextPin = anchors.find(a => data.histories.findIndex((h: string) => h === a.href) < 0);
+            
+            if (nextPin) {
+                await browser.storage.local.set({ drawingData: data });
+                document.location.href = nextPin.href;
+                return;
+            }
         }
+
+        //TODO: 一覧を表示して終了させる
+        console.log("次のピンが見つからない");
 
     }, 6000);
 }
