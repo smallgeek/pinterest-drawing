ed851b2dd78e3fd3bd7d1001bfaba0bf0cfdc088
smallgeek <smallgeek@outlook.jp>
2020-07-31 09:21:03 +0900

    残り時間を7セグメント表示にした

--- src/components/PDTime.vue
+++ src/components/PDTime.vue
@@ -1,19 +1,73 @@
 <template>
-  <div>
-    <span>{{ time }}</span>
+  <div class="seven-segment-container">
+    <seven-segment :value="Math.floor(minute / 10)" 
+                   :rounded="false" 
+                   :segment-width="8" 
+                   :segment-height="2" 
+                   on-color="#e60023" 
+                   off-color="transparent" 
+                   class="seven-segment" />
+    <seven-segment :value="minute % 10" 
+                   :rounded="false" 
+                   :segment-width="8" 
+                   :segment-height="2" 
+                   on-color="#e60023" 
+                   off-color="transparent" 
+                   class="seven-segment" />
+    <div>
+      <span class="segment-separator">:</span>
+    </div>
+    <seven-segment :value="Math.floor(second / 10)" 
+                   :rounded="false" 
+                   :segment-width="8" 
+                   :segment-height="2" 
+                   on-color="#e60023" 
+                   off-color="transparent" 
+                   class="seven-segment" />
+    <seven-segment :value="second % 10" 
+                   :rounded="false" 
+                   :segment-width="8" 
+                   :segment-height="2"
+                   on-color="#e60023" 
+                   off-color="transparent" 
+                   class="seven-segment" />
   </div>
 </template>
 
+<style>
+  .seven-segment-container {
+    display: flex;
+    align-items: center;
+    padding: 0 10px;
+  }
+  .seven-segment {
+    margin: 0 1px;
+  }
+  .segment-separator {
+    font-size: 22px;
+    color: #e60023;
+  }
+</style>
+
 <script lang="ts">
 import moment from 'moment';
-import Vue from 'vue'
+import Vue from 'vue';
+import SevenSegmentDisplay from 'vue-seven-segment-display';
 
 export default Vue.extend({
   name: "PDTime",
+  components: {
+    "seven-segment": SevenSegmentDisplay
+  },
   props: { "ms" : Number },
   computed: {
-    time() {
-      return moment(this.ms).format("mm:ss");
+    minute() {
+      const m = moment(this.ms);
+      return m.minute();
+    },
+    second() {
+      const m = moment(this.ms);
+      return m.second();
     }
   }
 })
