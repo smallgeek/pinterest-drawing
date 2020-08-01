71a5c800fece1add0eeda39afcb3e27362d33786
smallgeek <smallgeek@outlook.jp>
2020-08-01 09:04:40 +0900

    設定ページのひな型を作成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   filenameHashing: false,
   productionSourceMap: false,
+
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'popup/popup.html', to: 'popup/popup.html', context: 'src' }
+      ]})
+    ]
+  }
 };
