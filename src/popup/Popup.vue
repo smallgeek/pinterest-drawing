1f6ca5608e5d2855b06e953906cd7275e397d2ac
smallgeek <smallgeek@outlook.jp>
2020-08-14 09:32:22 +0900

    回数の入力欄を追加

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -10,63 +10,69 @@
       </div>
     </div>
     <h3>基本設定</h3>
-    <div class="popup-config-category">切り替えの間隔</div>
-    <div class="popup-flex">
-      <!-- 30秒 -->
-      <!-- TODO コンポーネントにする -->
-      <div class="popup-radio">
-        <input id="30" name="interval" type="radio" value="30000" v-model.number="interval" @change="onChanged">
+    <div style="margin-bottom: 28px;">
+      <div class="popup-config-category">切り替えの間隔</div>
+      <div class="popup-flex">
+        <!-- 30秒 -->
+        <!-- TODO コンポーネントにする -->
+        <div class="popup-radio">
+          <input id="30" name="interval" type="radio" value="30000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="30">
+            <div>30秒</div>
+          </label>
+        </div>
+        <!-- 60秒 -->
+        <div class="popup-radio">
+          <input id="60" name="interval" type="radio" value="60000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="60">
+            <div>60秒</div>
+          </label>
+        </div>
+        <!-- 90秒 -->
+        <div class="popup-radio">
+          <input id="90" name="interval" type="radio" value="90000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="90">
+            <div>90秒</div>
+          </label>
+        </div>
+        <!-- 180秒 -->
+        <div class="popup-radio">
+          <input id="180" name="interval" type="radio" value="180000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="180">
+            <div>180秒(3分)</div>
+          </label>
+        </div>
+        <!-- 300秒 -->
+        <div class="popup-radio">
+          <input id="300" name="interval" type="radio" value="300000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="300">
+            <div>300秒(5分)</div>
+          </label>
+        </div>
+        <!-- 600秒 -->
+        <div class="popup-radio">
+          <input id="600" name="interval" type="radio" value="600000" v-model.number="interval" @change="onChanged">
+        </div>
+        <div class="popup-label">
+          <label for="600">
+            <div>600秒(10分)</div>
+          </label>
+        </div>
       </div>
-      <div class="popup-label">
-        <label for="30">
-          <div>30秒</div>
-        </label>
-      </div>
-      <!-- 60秒 -->
-      <div class="popup-radio">
-        <input id="60" name="interval" type="radio" value="60000" v-model.number="interval" @change="onChanged">
-      </div>
-      <div class="popup-label">
-        <label for="60">
-          <div>60秒</div>
-        </label>
-      </div>
-      <!-- 90秒 -->
-      <div class="popup-radio">
-        <input id="90" name="interval" type="radio" value="90000" v-model.number="interval" @change="onChanged">
-      </div>
-      <div class="popup-label">
-        <label for="90">
-          <div>90秒</div>
-        </label>
-      </div>
-      <!-- 180秒 -->
-      <div class="popup-radio">
-        <input id="180" name="interval" type="radio" value="180000" v-model.number="interval" @change="onChanged">
-      </div>
-      <div class="popup-label">
-        <label for="180">
-          <div>180秒(3分)</div>
-        </label>
-      </div>
-      <!-- 300秒 -->
-      <div class="popup-radio">
-        <input id="300" name="interval" type="radio" value="300000" v-model.number="interval" @change="onChanged">
-      </div>
-      <div class="popup-label">
-        <label for="300">
-          <div>300秒(5分)</div>
-        </label>
-      </div>
-      <!-- 600秒 -->
-      <div class="popup-radio">
-        <input id="600" name="interval" type="radio" value="600000" v-model.number="interval" @change="onChanged">
-      </div>
-      <div class="popup-label">
-        <label for="600">
-          <div>600秒(10分)</div>
-        </label>
-      </div>
+    </div>
+    <div style="margin-bottom: 28px;">
+      <div class="popup-config-category">回数</div>
+      <input class="popup-input" type="number">
     </div>
   </div>
 </template>
@@ -74,6 +80,7 @@
 <style>
   .popup-container {
     font-family: 'SF Pro JP,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,Helvetica,"\30D2\30E9\30AE\30CE\89D2\30B4  Pro W3",Hiragino Kaku Gothic Pro,"\30E1\30A4\30EA\30AA",Meiryo,"\FF2D\FF33  \FF30\30B4\30B7\30C3\30AF",Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol';
+    quotes: "\300C" "\300D";
     width: 700px;
     height: 400px;
     padding: 10px;
@@ -93,7 +100,6 @@
     margin-top: 0;
     font-size: 36px;
     color: #111;
-    quotes: "\300C" "\300D";
     text-align: left;
     flex-grow: 0;
   }
@@ -150,6 +156,24 @@
   .popup-label {
     padding: 0 4px;
     margin-right: 12px;
+  }
+  .popup-input {
+    margin: 0;
+    vertical-align: middle;
+    line-height: normal;
+    appearance: none;
+    border-radius: 16px;
+    border-style: solid;
+    border-width: 2px;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+    width: 100%;
+    padding: 8px 16px;
+    border-color: #ddd;
+    background-color: #fff;
+    color: #111;
+    min-height: 48px;
+    box-sizing: border-box;
   }
 </style>
 
